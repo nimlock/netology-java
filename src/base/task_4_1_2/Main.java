b3334e5b253bd97944c384b2bde7aae71831da3c
Жиляев Иван Викторович <zhilyaev.ivan@itmh.ru>
2021-03-31 11:17:00 +0500

    Выполнена задача 4-1-2

---
+++ src/base/task_4_1_2/Main.java
@@ -0,0 +1,12 @@
+package base.task_4_1_2;
+
+public class Main {
+    public static void main(String[] args) {
+        // параметризируем класс типом String для ключа и значения
+        Box<String, String> sample1 = new Box<>("имя", "Нетология");
+        System.out.println(sample1);
+        // параметризируем класс типом Integer для ключа и Boolean для значения
+        Box<Integer, Boolean> sample2 = new Box<>(1, Boolean.TRUE);
+        System.out.println(sample2);
+    }
+}
